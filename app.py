dd20d7e33fdb00cd19313f0fa3d55c8445bc863c
Igor Finagin <Igor@Finag.in>
2020-03-17 23:35:58 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import json
+import logging
+import sqlite3
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+logging.basicConfig(filename='logs.log', format=FORMAT)
+d = {'clientip': '192.168.0.1', 'user': 'fbloggs'}
+logger = logging.getLogger('tcpserver')
+logger.info('%s', 'Start App', extra=d)
+
+table = 'requests'
+
+conn = sqlite3.connect("db.sqlite3")
+try:
+    cursor = conn.cursor()
+    cursor.execute(f"""CREATE TABLE {table}
+                       (data text, args text)
+                   """)
+    conn.commit()
+except sqlite3.OperationalError:
+    logger.warning('table "%s" already exists', table, extra=d)
+finally:
+    conn.close()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    conn = sqlite3.connect("mydatabase.sqlite")
+    cursor = conn.cursor()
+
+    try:
+        try:
+            data = [
+                (
+                    request.data,
+                    json.dumps(request.args),
+                ),
+            ]
+        except:
+            logger.warning('Can\'t json dump', extra=d)
+
+        cursor.executemany(f"INSERT INTO {table} VALUES (?, ?)", data)
+        conn.commit()
+    except:
+        logger.warning('Can\'t insert into "%s"', table, extra=d)
+    return json.dumps(request.args)
+    # return request.data
